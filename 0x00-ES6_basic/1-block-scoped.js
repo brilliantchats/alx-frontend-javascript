cf0abc7f01e2e80a92f62fd03780ed04eced6601
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-07-25 02:21:38 +0000

    eslint and variable scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    // pass
   }
 
   return [task, task2];
