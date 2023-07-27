fbfdea61001a23d7f65caf777737a0dd3607a7ba
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-07-27 00:24:13 +0000

    Javascript promises

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -6,5 +6,10 @@ export default function getSanFranciscoDescription() {
     capita: '$178,479',
   };
 
-  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp} , and a GDP per capita of ${budget.capita}.`;
+  const text1 = `As of ${year}, it was the seventh-highest income county in the United States, `;
+  const text2 = `with a per capita personal income of ${budget.income}. As of 2015, San Francisco `;
+  const text3 = `proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+  const resultStr = text1 + text2 + text3;
+
+  return resultStr;
 }
---
+++ 0x01-ES6_promise/README.md
@@ -0,0 +1,2 @@
+# PROMISES
+This is part of the ALX software engineering curriculum on frontend programming. This particular tutorial is on javascript promises for asynchronous programming
