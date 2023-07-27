538fbb4c91e7490fa66888a99bdeaf27a307a916
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-07-27 03:47:29 +0000

    promise handling

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'success' }))
-    .catch(() => Error())
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  }, () => {
+    console.log('Got a response from the API');
+    return Error();
+  });
 }
