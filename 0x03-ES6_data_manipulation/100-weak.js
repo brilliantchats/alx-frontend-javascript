4ef51dd5f19287cd48d12ea2c676d910e25880a8
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-08-02 01:20:31 +0000

    weak map

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,6 +1,7 @@
 export const weakMap = new WeakMap();
 export function queryAPI(obj) {
   if (weakMap.get(obj) >= 5) {
+    weakMap.set(obj, (weakMap.get(obj) || 0) + 1);
     throw new Error('Endpoint load is high');
   } else {
     weakMap.set(obj, (weakMap.get(obj) || 0) + 1);
