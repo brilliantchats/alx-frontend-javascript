fedcc7c68ff676a53cb2ddbd0aa650b657023a05
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-08-02 01:37:48 +0000

    sets and strings

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,12 @@ export default function cleanSet(set, startString) {
   const strLen = startString.length;
   const resultArr = [];
   for (const value of set) {
-    if (value.startsWith(startString)) {
+    if (value.startsWith(startString) && startString !== '') {
       resultArr.push(value.slice(strLen));
     }
   }
-  return resultArr.join('-');
+  if (resultArr.length > 0) {
+    return resultArr.join('-');
+  }
+  return '';
 }
